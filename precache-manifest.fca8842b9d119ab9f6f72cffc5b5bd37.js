99ee7f0c4070fb123aaaae966bd8520f729b9ad2
rita3ko <rita@cloudflare.com>
2020-12-11 10:02:12 -0800

    Updates

---
+++ precache-manifest.fca8842b9d119ab9f6f72cffc5b5bd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d28fac4b5bc3e7b48c4f56ae791e2dc",
+    "url": "/charades-ui/index.html"
+  },
+  {
+    "revision": "9f0e16be86248106025f",
+    "url": "/charades-ui/static/css/main.e1ec97c8.chunk.css"
+  },
+  {
+    "revision": "5e94048b65a950fc365f",
+    "url": "/charades-ui/static/js/2.4a686c77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/charades-ui/static/js/2.4a686c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f0e16be86248106025f",
+    "url": "/charades-ui/static/js/main.efed440d.chunk.js"
+  },
+  {
+    "revision": "be44eac530dbd6507506",
+    "url": "/charades-ui/static/js/runtime-main.c5fdbdd8.js"
+  }
+]);
